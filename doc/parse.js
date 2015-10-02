9a8890f934cdd93d94346ac654bf238ca594e97d
Danny Nemer <hi@DannyNemer.com>
2015-10-02 13:11:37 -0400

    Tweak comments in 'parse.js'

--- doc/parse.js
+++ doc/parse.js
@@ -6,12 +6,14 @@ var repoRootPath = __dirname + '/../'
 
 // Generate README from JSDoc.
 fs.writeFileSync(repoRootPath + 'README.md', docdown({
+	// Path to file to parse.
 	path: repoRootPath + package.main,
 	// Remove leading 'git+' and trailing '.git' from repository url.
 	url: package.repository.url.slice(4, -4) + '/blob/master/' + package.main,
+	// Heading title for README.
 	title: package.name,
 	// Load README introduction from 'introduction.md'.
-	description: fs.readFileSync(__dirname + '/introduction.md', 'utf8'),
+	introduction: fs.readFileSync(__dirname + '/introduction.md', 'utf8'),
 	// Do not alphabetically sort functions and categories.
 	sort: false,
 }))
